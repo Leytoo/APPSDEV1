b379a171a1d02cbf9ad123fde4083b902afb74e2
Li2mee <panimdim217@gmail.com>
2025-11-30 17:05:37 +0800

    Add files via upload

---
+++ src/components/ProfilePicture.jsx
@@ -0,0 +1,13 @@
+import profileImg from '../assets/profile.png';
+
+const ProfilePicture = () => {
+  return (
+    <div className="profile-picture-wrapper">
+      <div className="profile-picture">
+        <img src="1.jpg" />
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePicture;
